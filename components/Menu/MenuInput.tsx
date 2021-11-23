4ea963d1afaa9e21b90a9c67b0668cbdc4665143
nu0ma <ishikawaryoufutoshi@ishikawaryoufutoshinoMacBook-Air.local>
2021-11-23 12:15:01 +0900

    feat: add form validation

--- components/Menu/MenuInput.tsx
+++ components/Menu/MenuInput.tsx
@@ -1,17 +1,80 @@
-import { Box, Button, Flex, FormLabel, Input } from '@chakra-ui/react';
+import {
+  Box,
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
+  useToast,
+} from '@chakra-ui/react';
+
+import { useForm } from 'react-hook-form';
+
+type FormInputs = {
+  menu: string;
+  weight: string;
+  rep: string;
+};
 
 export const MenuInput = () => {
-  const handleSubmit = () => {};
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: 'all' });
+
+  const toast = useToast();
+
+  const onSubmit = (data: FormInputs) => {
+    console.log(data);
+    toast({
+      title: '追加',
+      status: 'success',
+      duration: 900,
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)} noValidate>
       <Flex justifyContent="center" flexDirection="column" maxW="lg">
-        <FormLabel>種目</FormLabel>
-        <Input></Input>
-        <FormLabel>重量</FormLabel>
-        <Input></Input>
-        <FormLabel>レップ数</FormLabel>
-        <Input></Input>
-        <Button onSubmit={handleSubmit} mt="8" colorScheme="teal">
+        <FormControl isInvalid={errors.menu ? true : false}>
+          <FormLabel>種目</FormLabel>
+          <Input
+            autoComplete="off"
+            {...register('menu', {
+              required: '種目は必須です',
+            })}
+          ></Input>
+          <FormErrorMessage role="alert">
+            {errors.menu && errors.menu.message}
+          </FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={errors.weight ? true : false}>
+          <FormLabel>重量</FormLabel>
+          <Input
+            type="number"
+            {...register('weight', {
+              required: '重量を入力してください',
+            })}
+          ></Input>
+          <FormErrorMessage role="alert">
+            {errors.weight && errors.weight.message}
+          </FormErrorMessage>
+        </FormControl>
+
+        <FormControl isInvalid={errors.rep ? true : false}>
+          <FormLabel>レップ数</FormLabel>
+          <Input
+            type="number"
+            {...register('rep', {
+              required: 'レップ数を入力してください',
+            })}
+          ></Input>
+        </FormControl>
+
+        <Button type="submit" mt="8" colorScheme="teal">
           Submit
         </Button>
       </Flex>
