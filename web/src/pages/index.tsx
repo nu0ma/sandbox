2e6f3f5ee057420632fdeb6d35043395af951aea
nu0ma <igreen.re317v10@gmail.com>
2022-12-29 12:18:01 +0900

    fix: delete unused file

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -5,13 +5,7 @@ export default function Home() {
     <>
       <h1>Pages</h1>
       <div>
-        <Link href="/todo">Todo</Link>
-      </div>
-      <div>
         <Link href="/user">User(Async Data Fetching)</Link>
-      </div>
-      <div>
-        <Link href="/post/1">Post(Async Data Fetching with parameters)</Link>
       </div>
     </>
   );
