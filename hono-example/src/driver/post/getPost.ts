b77c4f609b3d97e8bc2ec8a18a1849f56ad1a93e
nu0ma <igreen.re317v10@gmail.com>
2024-01-28 16:42:26 +0900

    fix

--- hono-example/src/driver/post/getPost.ts
+++ hono-example/src/driver/post/getPost.ts
@@ -1,7 +1,9 @@
 import { Post } from "../../types/post";
 
+const baseUrl = "https://jsonplaceholder.typicode.com";
+
 export const fetchPost = async (id: string): Promise<Post> => {
-  const url = new URL(`posts/${id}`, "https://jsonplaceholder.typicode.com/");
+  const url = new URL(`/posts/${id}`, baseUrl);
   const res = await fetch(url.toString());
   const json = await res.json();
 
