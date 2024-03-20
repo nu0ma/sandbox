18c054d00f49330b2d33319d86f3c46e25314211
nu0ma <igreen.re317v10@gmail.com>
2024-03-20 19:12:54 +0900

    graceful shutdown

--- go-playground/trial-echo/main.go
+++ go-playground/trial-echo/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/nu0ma/sandbox/go-playground/trial-echo/middleware"
@@ -20,6 +24,19 @@ func main() {
 
 	e.GET("/todo", rest.GetTodo)
 
-	e.Logger.Fatal(e.Start(":1323"))
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
+	go func() {
+		if err := e.Start(":1323"); err != nil && err != http.ErrServerClosed {
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	<-ctx.Done()
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
 }
