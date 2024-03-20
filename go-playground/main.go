25cb956647563d29356e3962de8ccd3c35a04896
nu0ma <igreen.re317v10@gmail.com>
2024-03-20 09:53:16 +0900

    fanIn

--- go-playground/main.go
+++ go-playground/main.go
@@ -5,6 +5,22 @@ import (
 	"time"
 )
 
+func fanIn(ch1, ch2 <-chan string) <-chan string {
+	new_ch := make(chan string)
+	go func() {
+		for {
+			new_ch <- <-ch1
+		}
+	}()
+	go func() {
+		for {
+			new_ch <- <-ch2
+		}
+	}()
+
+	return new_ch
+}
+
 func generator(msg string) <-chan string {
 	ch := make(chan string)
 	go func() {
@@ -18,8 +34,8 @@ func generator(msg string) <-chan string {
 }
 
 func main() {
-	ch := generator("Hello")
-	for i := 0; i < 5; i++ {
+	ch := fanIn(generator("Hello"), generator("Bye"))
+	for i := 0; i < 10; i++ {
 		fmt.Println(<-ch)
 	}
 }
