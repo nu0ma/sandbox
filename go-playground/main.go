cadeac3685508548baf69ba5520cd646c2eaa3f9
nu0ma <igreen.re317v10@gmail.com>
2024-03-20 09:19:37 +0900

    first

--- go-playground/main.go
+++ go-playground/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"sync"
+	"time"
+)
+
+func doSomething(wg *sync.WaitGroup) {
+	defer wg.Done()
+	time.Sleep(100 * time.Second)
+}
 
 func main() {
-	fmt.Println("Hello World")
+	var wg sync.WaitGroup
+
+	for i:=0; i<100; i++ {
+		wg.Add(1)
+		go doSomething(&wg)
+	}
+	wg.Wait()
 }
