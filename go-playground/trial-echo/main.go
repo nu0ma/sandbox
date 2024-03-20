02d9e29b72b7bafdc221037dc4600f525df827af
nu0ma <igreen.re317v10@gmail.com>
2024-03-20 17:41:50 +0900

    delete handler

--- go-playground/trial-echo/main.go
+++ go-playground/trial-echo/main.go
@@ -4,7 +4,7 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
-	"github.com/nu0ma/sandbox/go-playground/trial-echo/handler"
+	rest "github.com/nu0ma/sandbox/go-playground/trial-echo/rest/todo"
 )
 
 func main() {
@@ -14,7 +14,7 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "hello")
 	})
-	e.GET("/todo", handler.GetTodo)
+	e.GET("/todo", rest.GetTodo)
 
 	e.Logger.Fatal(e.Start(":1323"))
 
--- go-playground/trial-echo/rest/todo/get.go
+++ go-playground/trial-echo/rest/todo/get.go
@@ -1,21 +1,22 @@
 package rest
 
 import (
+	"net/http"
+
 	"github.com/labstack/echo/v4"
-	"github.com/nu0ma/sandbox/go-playground/trial-echo/domain"
 	"github.com/nu0ma/sandbox/go-playground/trial-echo/driver"
 	"github.com/nu0ma/sandbox/go-playground/trial-echo/gateway"
 	usecase "github.com/nu0ma/sandbox/go-playground/trial-echo/usecase/todo"
 )
 
-func Get(e echo.Context) (*domain.Todo, error) {
+func GetTodo(e echo.Context) error {
 	driver := driver.NewDBDriver()
 	gateway := gateway.NewTodoGateway(driver)
 	usecase := usecase.NewTodoUsecase(gateway)
 	resp, err := usecase.GetTodo(e)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	return resp, nil
+	return e.JSON(http.StatusOK, resp)
 }
