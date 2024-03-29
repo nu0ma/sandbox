8f29a23b19659502b1356df56d3260d42bdf4927
nu0ma <igreen.re317v10@gmail.com>
2024-03-29 23:52:28 +0900

    refactor

---
+++ go-playground/concurrency-playground/concurrency-3/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+func main() {
+	tasks := []int{
+		1, 2, 3, 4, 5,
+	}
+
+	var wg sync.WaitGroup
+	wg.Add(5)
+
+	for _, tasks := range tasks {
+		go func() {
+			fmt.Println(tasks)
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+}
