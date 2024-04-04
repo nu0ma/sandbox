235b3a7e43c46c28919a9d5a875383ddd0cf683e
nu0ma <igreen.re317v10@gmail.com>
2024-04-04 21:44:06 +0900

    その2

--- go-playground/concurrency-playground/concurrency-3/main.go
+++ go-playground/concurrency-playground/concurrency-3/main.go
@@ -7,27 +7,30 @@ import (
 
 func main() {
 	ch1 := make(chan int)
+	ch2 := make(chan int)
 
-	// 5秒待ってからチャネルを閉じる
 	go func() {
-		fmt.Printf("go routine...\n")
-		time.Sleep(5 * time.Second)
-		close(ch1)
+		time.Sleep(time.Millisecond)
+		ch1 <- 1
 	}()
 
-	counter := 0
+	go func() {
+		time.Sleep(time.Second * 2)
+		ch2 <- 2
+	}()
 
-LOOP:
+loop:
 	for {
 		select {
-		case <-ch1: // チャネルが閉じたらループから抜ける、その間に他のことができる
-			break LOOP
+		case v := <-ch1:
+			fmt.Printf("ch1:%v", v)
+			break loop
+		case v := <-ch2:
+			fmt.Printf("ch2:%v", v)
+			break loop
 		default:
+			fmt.Printf("progress....")
 		}
-
-		counter++ // チャネルが閉じるまでここでcounter++で数値を増やす。並行に動作するGは5秒かかるので5秒分つまりcounterに5が入る。
-		time.Sleep(1 * time.Second)  //
 	}
 
-	fmt.Printf("%v\n", counter)
 }
