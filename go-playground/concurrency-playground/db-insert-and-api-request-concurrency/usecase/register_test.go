bd585cb1fb169d1843a0c2fd267264f4680f8cad
nu0ma <igreen.re317v10@gmail.com>
2024-04-16 22:22:24 +0900

    create test

--- go-playground/concurrency-playground/db-insert-and-api-request-concurrency/usecase/register_test.go
+++ go-playground/concurrency-playground/db-insert-and-api-request-concurrency/usecase/register_test.go
@@ -2,11 +2,17 @@ package usecase
 
 import (
 	"db-insert-and-api-request-concurrency/driver"
+	"db-insert-and-api-request-concurrency/mocks"
 	"testing"
 )
 
 func TestRegisterUsecase_Register(t *testing.T) {
-	// TODO
+	mockDBDriver := mocks.NewDBDriverInterface(t)
+	mockAPIDriver := mocks.NewAPIDriverInterface(t)
+
+	mockDBDriver.EXPECT().Post().Return(nil)
+	mockAPIDriver.EXPECT().Post().Return(nil)
+
 	type fields struct {
 		dbDriver  driver.DBDriverInterface
 		apiDriver driver.APIDriverInterface
@@ -20,7 +26,17 @@ func TestRegisterUsecase_Register(t *testing.T) {
 		args    args
 		wantErr bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "Test Register",
+			fields: fields{
+				dbDriver:  mockDBDriver,
+				apiDriver: mockAPIDriver,
+			},
+			args: args{
+				done: make(chan interface{}),
+			},
+			wantErr: false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
