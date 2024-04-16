6e0bf50522659309370db741ad46b1b8a26ea50a
nu0ma <igreen.re317v10@gmail.com>
2024-04-16 22:29:05 +0900

    create test 2

--- go-playground/concurrency-playground/db-insert-and-api-request-concurrency/usecase/register_test.go
+++ go-playground/concurrency-playground/db-insert-and-api-request-concurrency/usecase/register_test.go
@@ -1,48 +1,65 @@
 package usecase
 
 import (
-	"db-insert-and-api-request-concurrency/driver"
 	"db-insert-and-api-request-concurrency/mocks"
+	"errors"
 	"testing"
 )
 
 func TestRegisterUsecase_Register(t *testing.T) {
-	mockDBDriver := mocks.NewDBDriverInterface(t)
-	mockAPIDriver := mocks.NewAPIDriverInterface(t)
-
-	mockDBDriver.EXPECT().Post().Return(nil)
-	mockAPIDriver.EXPECT().Post().Return(nil)
 
 	type fields struct {
-		dbDriver  driver.DBDriverInterface
-		apiDriver driver.APIDriverInterface
+		dbReturn  error
+		apiReturn error
 	}
 	type args struct {
 		done <-chan interface{}
 	}
 	tests := []struct {
-		name    string
-		fields  fields
-		args    args
-		wantErr bool
+		name      string
+		fields    fields
+		args      args
+		wantErr   bool
+		dbReturn  error
+		apiReturn error
 	}{
 		{
-			name: "Test Register",
-			fields: fields{
-				dbDriver:  mockDBDriver,
-				apiDriver: mockAPIDriver,
-			},
-			args: args{
-				done: make(chan interface{}),
-			},
-			wantErr: false,
+			name:      "Test Register",
+			fields:    fields{dbReturn: nil, apiReturn: nil},
+			args:      args{done: make(chan interface{})},
+			wantErr:   false,
+			dbReturn:  nil,
+			apiReturn: nil,
+		},
+		{
+			name:      "Test Register with error",
+			fields:    fields{dbReturn: errors.New("error"), apiReturn: nil},
+			args:      args{done: make(chan interface{})},
+			wantErr:   true,
+			dbReturn:  errors.New("error"),
+			apiReturn: nil,
+		},
+		{
+			name:      "Test Register with error",
+			fields:    fields{dbReturn: nil, apiReturn: errors.New("error")},
+			args:      args{done: make(chan interface{})},
+			wantErr:   true,
+			dbReturn:  nil,
+			apiReturn: errors.New("error"),
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+
+			mockDBDriver := mocks.NewDBDriverInterface(t)
+			mockAPIDriver := mocks.NewAPIDriverInterface(t)
+
+			mockDBDriver.EXPECT().Post().Return(tt.fields.dbReturn)
+			mockAPIDriver.EXPECT().Post().Return(tt.apiReturn)
+
 			u := &RegisterUsecase{
-				dbDriver:  tt.fields.dbDriver,
-				apiDriver: tt.fields.apiDriver,
+				dbDriver:  mockDBDriver,
+				apiDriver: mockAPIDriver,
 			}
 			if err := u.Register(tt.args.done); (err != nil) != tt.wantErr {
 				t.Errorf("RegisterUsecase.Register() error = %v, wantErr %v", err, tt.wantErr)
