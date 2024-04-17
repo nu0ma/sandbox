110423d2ebd3ad1109302e72473515046fe9a5ad
nu0ma <igreen.re317v10@gmail.com>
2024-04-17 23:31:07 +0900

    functional option pattern

--- go-playground/sandbox/main.go
+++ go-playground/sandbox/main.go
@@ -1,49 +1,8 @@
 package main
 
-import (
-	"errors"
-	"fmt"
-)
-
-type Config struct {
-	Port int
-}
-
-type ConfigBuilder struct {
-	port *int
-}
-
-func (b *ConfigBuilder) Port(port int) *ConfigBuilder {
-	b.port = &port
-	return b
-}
-
-func (b *ConfigBuilder) Build() (Config, error) {
-	cfg := Config{}
-
-	if b.port == nil {
-		cfg.Port = 80
-	} else if *b.port < 0 {
-		return Config{}, errors.New("port should be positive")
-	} else {
-		cfg.Port = *b.port
-	}
-	return cfg, nil
-}
+import "sandbox/server"
 
 func main() {
-	builder1 := ConfigBuilder{}
-	builder1.Port(9000)
-	cfg, err := builder1.Build()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(cfg.Port)
-
-	builder2 := ConfigBuilder{}
-	cfg2, err := builder2.Build()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(cfg2.Port)
+	s1 := server.New("localhost", 8080)
+	s2 := server.New("localhost", 90, server.WithTimeout(80))
 }
