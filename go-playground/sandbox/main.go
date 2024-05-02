665493ddd0fd0258586716e84c363a1bafe0d068
nu0ma <igreen.re317v10@gmail.com>
2024-05-02 22:56:30 +0900

    slice memory leak : fix

--- go-playground/sandbox/main.go
+++ go-playground/sandbox/main.go
@@ -7,8 +7,9 @@ func main() {
 	fmt.Println(len(s1))
 	fmt.Println(cap(s1))
 
-	s2 := s1[:5]
+	s2 := make([]int, 5)
+	copy(s2, s1)
 
 	fmt.Println(len(s2))
-	fmt.Println(cap(s2)) //100000000
+	fmt.Println(cap(s2)) //5
 }
