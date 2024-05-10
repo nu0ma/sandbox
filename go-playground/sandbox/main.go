d8e2d2dfcf5dc40fb03b2c4ea0d26e6ff756553d
nu0ma <igreen.re317v10@gmail.com>
2024-05-10 09:27:13 +0900

    rune fix

--- go-playground/sandbox/main.go
+++ go-playground/sandbox/main.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	s := "漢"
-	fmt.Println(len(s)) // 3
+	s := "hållo"
+	fmt.Println(len(s)) // 6
+
+	runes := []rune(s)
+	for i, r := range runes {
+		fmt.Printf("%d,%c\n", i, r)
+	}
 }
